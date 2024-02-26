82817d3641cafc82952e2335c30dbf91fb1c7efb
Levi King <leviaking@gmail.com>
2024-02-26 12:32:08 -0500

    3-letter words
    3-letter words for use in 2024-02-25 Sunday Puzzle

---
+++ 2024-02-25-genie.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+## 2024/02/26. Levi K. From the blog,
+## https://naturallanguagepuzzling.blogspot.com
+## This code was written to assist with the 2024/02/25 Sunday Puzzle:
+## https://www.npr.org/2024/02/25/1233435347/sunday-puzzle-hidden-figures-in-two-word-phrases
+
+"""
+This week's challenge: This week's challenge comes to us from listener 
+Eric Berlin of Milford, Connecticut. Take the word SETS. You can add a 
+three-letter word to this twice to get a common phrase: SPARE PARTS. Can 
+you now do this with the word GENIE, add a three-letter word to it twice 
+to get a common phrase. Again, start with GENIE, insert a three-letter 
+word twice, get a common phrase.
+"""
+
+import string
